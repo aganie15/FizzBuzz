20b3e9b5b463a26eb7e1bca8e3445410a64d9ea6
aganie15 <aga.niewiadomska@softwire.com>
2022-09-07 13:56:26 +0100

    Implementation of part 1

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,18 @@
 def fizzbuzz
-  puts 'Hello world!'
+  for i in 1..100
+    multiple_of_three = (i % 3 == 0)
+    multiple_of_five = (i % 5 == 0)
+
+    if multiple_of_three && !multiple_of_five
+      puts 'Fizz'
+    elsif !multiple_of_three && multiple_of_five
+      puts 'Buzz'
+    elsif multiple_of_three && multiple_of_five
+      puts 'FizzBuzz'
+    else
+      puts i
+    end
+  end
 end
 
 fizzbuzz
